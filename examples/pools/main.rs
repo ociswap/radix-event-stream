ba5a314cc656ea72721f3bd18341bbe8dfaa6c6c
Krulknul <85589076+Krulknul@users.noreply.github.com>
2024-03-27 00:05:45 +0100

    give the example file capability

--- examples/pools/main.rs
+++ examples/pools/main.rs
@@ -16,13 +16,20 @@ fn main() {
     env::set_var("RUST_LOG", "info");
     env_logger::init();
 
+    let arg = match env::args().nth(1) {
+        Some(arg) => arg,
+        None => {
+            error!("Please provide the argument 'file' to run from a file, or 'gateway' to run from a gateway.");
+            std::process::exit(1);
+        }
+    };
+
     // Create a tokio runtime to run async code inside handlers.
     let runtime = tokio::runtime::Runtime::new().unwrap();
 
     // Create a new database and initialize a simple schema with
     // one table: `events` with "id" and "data" columns as integer and text
-    let database_url =
-        "sqlite:examples/pools_gateway/basicv0/my_database.db?mode=rwc";
+    let database_url = "sqlite:examples/pools/basicv0/my_database.db?mode=rwc";
     let pool = runtime.block_on(async {
         let pool: Pool<Sqlite> = SqlitePoolOptions::new()
             .max_connections(5)
@@ -52,14 +59,6 @@ fn main() {
         "package_rdx1p5l6dp3slnh9ycd7gk700czwlck9tujn0zpdnd0efw09n2zdnn0lzx",
         "InstantiateEvent",
         events::handle_instantiate_event,
-    );
-
-    // Create a new transaction stream, which the processor will use
-    // as a source of transactions.
-    let stream = GatewayTransactionStream::new(
-        1919391,
-        100,
-        "https://mainnet.radixdlt.com".to_string(),
     );
 
     // Define a generic handler for transactions,
@@ -96,17 +95,48 @@ fn main() {
         });
     }
 
-    // Start with parameters.
-    TransactionStreamProcessor::run_with(
-        stream,
-        handler_registry,
-        transaction_handler,
-        AppState {
-            number: 0,
-            async_runtime: Rc::new(runtime),
-            pool,
-            transaction: Arc::new(Mutex::new(None)),
-            network: scrypto::network::NetworkDefinition::mainnet(),
-        },
-    );
+    if arg == "file" {
+        // Create a new transaction stream from a file, which the processor will use
+        // as a source of transactions.
+        let stream =
+            radix_event_stream::sources::file::FileTransactionStream::new(
+                "examples/pools/transactions.json".to_string(),
+            );
+
+        // Start with parameters.
+        TransactionStreamProcessor::run_with(
+            stream,
+            handler_registry,
+            transaction_handler,
+            AppState {
+                number: 0,
+                async_runtime: Rc::new(runtime),
+                pool,
+                transaction: Arc::new(Mutex::new(None)),
+                network: scrypto::network::NetworkDefinition::mainnet(),
+            },
+        );
+    } else if arg == "gateway" {
+        // Create a new transaction stream, which the processor will use
+        // as a source of transactions.
+        let stream = GatewayTransactionStream::new(
+            1919391,
+            100,
+            "https://mainnet.radixdlt.com".to_string(),
+        );
+
+        // Start with parameters.
+        TransactionStreamProcessor::run_with(
+            stream,
+            handler_registry,
+            transaction_handler,
+            AppState {
+                number: 0,
+                async_runtime: Rc::new(runtime),
+                pool,
+                transaction: Arc::new(Mutex::new(None)),
+                network: scrypto::network::NetworkDefinition::mainnet(),
+            },
+        );
+    }
 }
--- src/sources/mod.rs
+++ src/sources/mod.rs
@@ -1,2 +1,3 @@
 pub mod file;
 pub mod gateway;
+pub mod string;
