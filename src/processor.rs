ba20b2c28400593e98f7ef3fc1cd96f45fd69261
Krulknul <85589076+Krulknul@users.noreply.github.com>
2024-04-19 19:40:59 +0200

    introduce logger

--- src/processor.rs
+++ src/processor.rs
@@ -4,20 +4,155 @@ use crate::{
         TransactionStreamProcessorError,
     },
     event_handler::{EventHandlerContext, HandlerRegistry},
-    models::Transaction,
+    models::{Event, Transaction},
     stream::TransactionStream,
     transaction_handler::{TransactionHandler, TransactionHandlerContext},
 };
 use async_trait::async_trait;
 use colored::Colorize;
-use log::{error, info};
-use std::time::{Duration, Instant};
+use log::{error, info, Log};
+use std::{
+    sync::{Arc, RwLock},
+    time::{Duration, Instant},
+};
 
 const CURRENT_STATE_REPORT_INTERVAL: u64 = 60;
 const TRANSACTION_RETRY_INTERVAL: u64 = 10;
 const EVENT_RETRY_INTERVAL: u64 = 10;
 
-/// Uses a `TransactionStream` to process transactions and
+pub trait Logger: Send + Sync {
+    fn before_handle_transaction(&self, transaction: &Transaction);
+    fn after_handle_transaction(
+        &self,
+        transaction: &Transaction,
+        time_spent: Duration,
+    );
+    fn before_handle_event(&self, transaction: &Transaction, event: &Event);
+    fn after_handle_event(
+        &self,
+        transaction: &Transaction,
+        event: &Event,
+        time_spent: Duration,
+    );
+    fn event_retry_error(
+        &self,
+        transaction: &Transaction,
+        event: &Event,
+        error: &anyhow::Error,
+        timeout: Duration,
+    );
+    fn transaction_retry_error(
+        &self,
+        transaction: &Transaction,
+        error: &anyhow::Error,
+        timeout: Duration,
+    );
+    fn unrecoverable_error(&self, error: &anyhow::Error);
+}
+
+pub struct DefaultLogger;
+
+impl Logger for DefaultLogger {
+    fn before_handle_transaction(&self, transaction: &Transaction) {
+        info!(
+            "{}",
+            "--------------------------------------------------------"
+                .bright_blue()
+        );
+        info!(
+            "{}",
+            format!(
+                "HANDLING TRANSACTION - {:#?} - {}",
+                transaction.state_version,
+                transaction.confirmed_at
+                    .expect("When handling a transaction it should always have a timestamp")
+                    .format("%a %d-%m-%Y %H:%M")
+            )
+            .bright_green()
+        );
+    }
+
+    fn after_handle_transaction(
+        &self,
+        transaction: &Transaction,
+        time_spent: Duration,
+    ) {
+        info!(
+            "{}",
+            format!(
+                "###### END TRANSACTION - HANDLED IN {}ms ######",
+                time_spent.as_millis()
+            )
+            .bright_green()
+        );
+        info!(
+            "{}",
+            "--------------------------------------------------------"
+                .bright_blue()
+        );
+    }
+
+    fn before_handle_event(&self, transaction: &Transaction, event: &Event) {
+        info!(
+            "{}",
+            format!("HANDLING EVENT: {}", event.name).bright_yellow()
+        );
+    }
+
+    fn after_handle_event(
+        &self,
+        transaction: &Transaction,
+        event: &Event,
+        time_spent: Duration,
+    ) {
+    }
+
+    fn event_retry_error(
+        &self,
+        transaction: &Transaction,
+        event: &Event,
+        error: &anyhow::Error,
+        timeout: Duration,
+    ) {
+        error!(
+            "{}",
+            format!("ERROR HANDLING EVENT: {} - {:?}", event.name, error)
+                .bright_red()
+        );
+        info!(
+            "{}",
+            format!("RETRYING IN {:.2} SECONDS\n", timeout.as_secs_f32())
+                .bright_yellow()
+        );
+    }
+
+    fn transaction_retry_error(
+        &self,
+        transaction: &Transaction,
+        error: &anyhow::Error,
+        timeout: Duration,
+    ) {
+        error!(
+            "{}",
+            format!("FATAL ERROR HANDLING TRANSACTION: {:?}\n", error)
+                .bright_red()
+        );
+        info!(
+            "{}",
+            format!("RETRYING IN {:.2} SECONDS\n", timeout.as_secs_f32())
+                .bright_yellow()
+        );
+    }
+
+    fn unrecoverable_error(&self, error: &anyhow::Error) {
+        error!(
+            "{}",
+            format!("UNRECOVERABLE ERROR: {:?}", error).bright_red()
+        );
+    }
+}
+
+/// Uses a `TransactionStream` to procoess transactions and
 /// events using a `HandlerRegistry`. Register event handlers
 /// using the `HandlerRegistry` and then call `run` to start
 /// processing transactions.
@@ -34,6 +169,8 @@ where
     transaction_retry_delay: Duration,
     event_retry_delay: Duration,
     current_state_report_interval: Duration,
+    current_state: Arc<RwLock<Option<u64>>>,
+    logger: Option<Box<dyn Logger>>,
 }
 
 #[allow(non_camel_case_types)]
@@ -60,6 +197,8 @@ where
             current_state_report_interval: Duration::from_millis(
                 CURRENT_STATE_REPORT_INTERVAL,
             ),
+            current_state: Arc::new(RwLock::new(None)),
+            logger: None,
         }
     }
 
@@ -120,22 +259,10 @@ where
             // we can skip processing it.
             return Ok(());
         }
-        info!(
-            "{}",
-            "--------------------------------------------------------"
-                .bright_blue()
-        );
-        info!(
-            "{}",
-            format!(
-                "HANDLING TRANSACTION - {:#?} - {}",
-                transaction.state_version,
-                transaction.confirmed_at
-                    .expect("When handling a transaction it should always have a timestamp")
-                    .format("%a %d-%m-%Y %H:%M")
-            )
-            .bright_green()
-        );
+
+        if let Some(logger) = &self.logger {
+            logger.before_handle_transaction(transaction);
+        }
 
         // Keep trying to handle the transaction in case
         // the handler requests this through a TransactionHandlerError.
@@ -147,6 +274,7 @@ where
                 event_processor: &mut EventProcessor {
                     event_retry_interval: self.event_retry_delay,
                     transaction,
+                    logger: &self.logger,
                 },
                 handler_registry: &mut self.handler_registry,
             })
@@ -154,60 +282,36 @@ where
         {
             match err {
                 TransactionHandlerError::TransactionRetryError(e) => {
-                    error!(
-                        "{}",
-                        format!("ERROR HANDLING TRANSACTION: {}", e)
-                            .bright_red()
-                    );
-                    info!(
-                        "{}",
-                        format!(
-                            "RETRYING TRANSACTION IN {:.2} SECONDS\n",
-                            self.transaction_retry_delay.as_secs_f32()
-                        )
-                        .bright_yellow()
-                    );
+                    if let Some(logger) = &self.logger {
+                        logger.transaction_retry_error(
+                            transaction,
+                            &e,
+                            self.transaction_retry_delay,
+                        );
+                    }
                     tokio::time::sleep(self.transaction_retry_delay).await;
-                    info!(
-                        "{}",
-                        format!(
-                            "RETRYING TRANSACTION - {:#?} - {}",
-                            transaction.state_version,
-                            transaction.confirmed_at
-                                .expect("When handling a transaction it should always have a timestamp")
-                                .to_rfc3339()
-                        )
-                        .bright_yellow()
-                    );
+                    if let Some(logger) = &self.logger {
+                        logger.before_handle_transaction(transaction);
+                    }
                     continue;
                 }
-                TransactionHandlerError::UnrecoverableError(err) => {
-                    error!(
-                        "{}",
-                        format!("FATAL ERROR HANDLING TRANSACTION: {}\n", err)
-                            .bright_red()
-                    );
+                TransactionHandlerError::UnrecoverableError(e) => {
+                    if let Some(logger) = &self.logger {
+                        logger.unrecoverable_error(&e);
+                    }
                     return Err(
-                        TransactionStreamProcessorError::UnrecoverableError(
-                            err,
-                        ),
+                        TransactionStreamProcessorError::UnrecoverableError(e),
                     );
                 }
             }
         }
-        info!(
-            "{}",
-            format!(
-                "###### END TRANSACTION - HANDLED IN {}ms ######",
-                before.elapsed().as_millis()
-            )
-            .bright_green()
-        );
-        info!(
-            "{}",
-            "--------------------------------------------------------"
-                .bright_blue()
-        );
+        if let Some(logger) = &self.logger {
+            logger.after_handle_transaction(transaction, before.elapsed());
+        }
+        self.current_state
+            .write()
+            .expect("Should be able to write the current state")
+            .replace(transaction.state_version);
         Ok(())
     }
 
@@ -272,6 +376,7 @@ where
 pub struct EventProcessor<'a> {
     event_retry_interval: Duration,
     transaction: &'a Transaction,
+    logger: &'a Option<Box<dyn Logger>>,
 }
 
 #[allow(non_camel_case_types)]
@@ -300,10 +405,11 @@ impl<'a> EventProcessor<'a> {
                     .unwrap()
             };
             let event_handler = event_handler.clone();
-            info!(
-                "{}",
-                format!("HANDLING EVENT: {}", event.name).bright_yellow()
-            );
+            let mut before: Option<Instant> = None;
+            if let Some(logger) = self.logger {
+                before = Some(Instant::now());
+                logger.before_handle_event(self.transaction, event);
+            }
             while let Err(err) = event_handler
                 .handle(
                     EventHandlerContext {
@@ -319,31 +425,31 @@ impl<'a> EventProcessor<'a> {
             {
                 match err {
                     EventHandlerError::EventRetryError(e) => {
-                        error!(
-                            "{}",
-                            format!("ERROR HANDLING EVENT: {}", e).bright_red()
-                        );
-
-                        info!(
-                            "{}",
-                            format!(
-                                "RETRYING IN {:.2} SECONDS\n",
-                                self.event_retry_interval.as_secs_f32()
-                            )
-                            .bright_yellow()
-                        );
+                        if let Some(logger) = self.logger {
+                            logger.event_retry_error(
+                                self.transaction,
+                                event,
+                                &e,
+                                self.event_retry_interval,
+                            );
+                        }
                         tokio::time::sleep(self.event_retry_interval).await;
-                        info!(
-                            "{}",
-                            format!("RETRYING HANDLING EVENT: {}", event.name)
-                                .bright_yellow()
-                        );
+                        if let Some(logger) = self.logger {
+                            logger.before_handle_event(self.transaction, event);
+                        }
                         continue;
                     }
                     _ => {
                         return Err(err);
                     }
                 }
+            }
+            if let Some(logger) = self.logger {
+                logger.after_handle_event(
+                    self.transaction,
+                    event,
+                    before.unwrap().elapsed(),
+                );
             }
         }
         Ok(())
