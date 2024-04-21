66ee7f63c0017c31dda875fab060b07bac269820
Krulknul <85589076+Krulknul@users.noreply.github.com>
2024-04-21 16:01:47 +0200

    fix some broken links in doc

--- src/macros.rs
+++ src/macros.rs
@@ -1,7 +1,8 @@
 //! Re-exports proc macros for defining event and transaction handlers.
 //!
 //! These macros convert async functions with a correct signature
-//! into a struct that implements the [`EventHandler`] or [`TransactionHandler`]
+//! into a struct that implements the [`EventHandler`][crate::event_handler::EventHandler]
+//! or [`TransactionHandler`][crate::transaction_handler::TransactionHandler]
 //! trait.
 
 pub use handler_macro::event_handler;
--- src/stream.rs
+++ src/stream.rs
@@ -8,9 +8,9 @@ use tokio::sync::mpsc::Receiver;
 
 /// A trait that abstracts a stream of transactions coming
 /// from any source, like a gateway, database, or file.
-/// The stream is started by calling [`TransactionStream::start`], which returns
-/// a [`tokio::sync::mpsc::Receiver`] that the [`TransactionStreamProcessor`] awaits
-/// on to receive transactions.
+/// The stream is started by calling [`TransactionStream::start`][crate::stream::TransactionStream], which returns
+/// a [`tokio::sync::mpsc::Receiver`] that the [`TransactionStreamProcessor`][crate::processor::TransactionStreamProcessor]
+/// awaits on to receive transactions.
 ///
 /// If the stream is finished, which can happen when processing
 /// a finite source of transactions such as a file, the stream
