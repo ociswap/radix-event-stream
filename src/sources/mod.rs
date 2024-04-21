c8f0ce3115ee9ac5b66ad2bdde88d5b2d0a04cc2
Krulknul <85589076+Krulknul@users.noreply.github.com>
2024-04-21 22:25:01 +0200

    make sources opt-in using feature flags

--- src/sources/mod.rs
+++ src/sources/mod.rs
@@ -1,7 +1,16 @@
 //! Implementations of [`TransactionStream`][crate::stream::TransactionStream] provided by
 //! the framework.
+//!
+//! It is possible to opt in to these implementations by enabling
+//! the corresponding feature flags. It is recommended to use feature flags
+//! to only include the implementations that are needed for your use case,
+//! because this allows you to skip some optional dependencies.
 
+#[cfg(feature = "channel")]
 pub mod channel;
+#[cfg(feature = "database")]
 pub mod database;
+#[cfg(feature = "file")]
 pub mod file;
+#[cfg(feature = "gateway")]
 pub mod gateway;
